a80e2288ff1202672f0330b6d7c943809a8ec5be
princegoswami5152R <princegoswami5152@gmail.com>
2026-05-08 14:37:26 +0530

    Correct naming

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>CP Profile Analyzer</h1>
+      <h1>CF Profile Analyzer</h1>
 
       <SearchBar onSearch={fetchUser} />
 
